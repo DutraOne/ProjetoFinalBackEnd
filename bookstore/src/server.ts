b3f3dc3b4481840221edf668a36fd737408826a6
DutraOne <lucasdfc10@outlook.com>
2024-11-20 21:20:51 -0300

    feat: adequação na parte do servidor

--- bookstore/src/server.ts
+++ bookstore/src/server.ts
@@ -3,7 +3,7 @@ import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
